0c975e29cdce85668bffe452adc6661ee40365a4
hrkd <hiroki@hrkd.net>
2015-06-10 12:29:50 +0900

    copy CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -376,6 +376,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
+            'CNAME',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
